fc3aecba16a794c6450750ed147fa811e4590f73
Javier Celada <celada.jcm.javier@gmail.com>
2024-10-24 10:16:57 +0200

    Icons

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import { GAMES } from "../../games";
 
 const Conatiner = styled.div`
     border: 1px solid;
@@ -24,6 +25,21 @@ const Label = styled.label`
     grid-column: 1 / 2;
 `;
 
+const Logo = styled.div`
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+    & > img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const Choose = styled.div`
+    grid-column: 2 / 2;
+    display: flex;
+`;
+
 const Select = styled.select`
     padding: 5px 10px;
     border-radius: 10px;
@@ -32,7 +48,6 @@ const Select = styled.select`
     box-shadow: 0 5px 25px rgba(0, 0, 0, 0.2);
     border: none;
     text-align: center;
-    grid-column: 2 / 2;
 `;
 
 const Option = styled.option`
@@ -68,48 +83,49 @@ const Button = styled.button`
     box-shadow: 0 5px 25px rgba(0, 0, 0, 0.2);
 `;
 
-const GAMES = [
-    {
-        name: "Marvel Rivals",
-        positions: ["Vanguard", "Duelist", "Strategist"]
-    },
-    {
-        name: "League Of Legends",
-        positions: ["Top", "Jungle", "Mid", "Adc", "Support"]
-    },
-    {
-        name: "Overwatch",
-        positions: ["Tank", "Damage", "Support"]
-    }
-];
-
 function Search() {
 
     const [game, setGame] = useState(GAMES[0]);
+    const [position, setPosition] = useState(game.positions[0]);
 
     const changeGame = (event) => {
         const game = GAMES.find(game => game.name === event.target.value);
         setGame(game);
+        setPosition(game.positions[0]);
+    };
+
+    const changePosition = (event) => {
+        const position = game.positions.find(position => position.name === event.target.value);
+        setPosition(position);
     };
 
     return (
         <Conatiner>
             <Row>
                 <Label>Game</Label>
-                <Select onChange={changeGame}>
-                    {GAMES.map(game => <Option key={game.name}>{game.name}</Option>)}
-                </Select>
+                <Choose>
+                    <Logo><img src={game.image} alt={game.image} /></Logo>
+                    <Select onChange={changeGame} value={game.name}>
+                        {GAMES.map(game =>
+                            <Option key={game.name}>
+                                {game.name}
+                            </Option>)}
+                    </Select>
+                </Choose>
             </Row>
             <Row>
                 <Label>Position</Label>
-                <Select>
-                    {game.positions.map(position => <Option key={position}>{position}</Option>)}
-                </Select>
+                <Choose>
+                    <Logo><img src={position.image} alt={position.image} /></Logo>
+                    <Select onChange={changePosition} value={position.name}>
+                        {game.positions.map(position => <Option key={position.name}>{position.name}</Option>)}
+                    </Select>
+                </Choose>
             </Row>
             <Row>
                 <Label>Day(s)</Label>
                 <Days>
-                    Mon<Day />Tue<Day />Wed<Day />Thu<Day />Fri<Day />Sat<Day />Sun<Day />
+                    <Day />Mon<Day />Tue<Day />Wen<Day />Thu<Day />Fri<Day />Sat<Day />Sun
                 </Days>
             </Row>
             <Row>
---
+++ src/games.js
@@ -0,0 +1,50 @@
+const OVERWATCH = {
+    name: "Overwatch",
+    image: "overwatch/logo.png",
+    positions: [
+        {
+            name: "Tank",
+            image: "overwatch/tank.png",
+        },
+        {
+            name: "Damage",
+            image: "overwatch/damage.png",
+        },
+        {
+            name: "Support",
+            image: "overwatch/support.png",
+        }
+    ]
+};
+
+const LEAGUE = {
+    name: "League Of Legends",
+    image: "league/logo.png",
+    positions: [
+        {
+            name: "Top",
+            image: "league/top.png",
+        },
+        {
+            name: "Jungle",
+            image: "league/jungle.png",
+        },
+        {
+            name: "Mid",
+            image: "league/mid.png",
+        },
+        {
+            name: "Bot",
+            image: "league/bot.png",
+        },
+        {
+            name: "Support",
+            image: "league/support.png",
+        }
+    ]
+};
+
+export const GAMES = [
+    OVERWATCH,
+    LEAGUE
+];
